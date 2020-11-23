20f6ce02b14f0755a23ce61ccb7abd2a1484cf97
kuro.shun <black.huke9630@gmail.com>
2020-11-23 15:37:19 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,37 @@
+<style>
+    .headerMywvLogo{
+        color: #464646;
+        font-weight: bold; 
+    }
+    
+    .nav-item-Mywv{
+        color: #464646;
+        font-weight: bold; 
+    }
+    
+    .mywvNavbar{
+        border-bottom: 1px solid #464646;
+    }
+    
+</style>
+
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm mywvNavbar">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand headerMywvLogo" href="/">MyWV</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item nav-item-Mywv"><a href="#" class="nav-link">新規登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item nav-item-Mywv"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
